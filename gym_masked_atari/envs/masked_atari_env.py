22330d62a60978f1c9eb6f77546ae4d477d196ba
Guy Davidson <guydav@gmail.com>
2020-03-27 12:05:16 -0400

    Made sure to (a) store the initial observation shape, and (b) modify it to the augmented observation shape

--- gym_masked_atari/envs/masked_atari_env.py
+++ gym_masked_atari/envs/masked_atari_env.py
@@ -1,8 +1,6 @@
 import gym
 import numpy as np
 from .frostbite_masker import FROSTBITE_MASKER_DEFINITIONS
-from gym import error, spaces, utils
-from gym.utils import seeding
 
 
 class MaskedAtariEnv(gym.Env):
@@ -11,6 +9,7 @@ class MaskedAtariEnv(gym.Env):
 
     def __init__(self, **kwargs):
         super(MaskedAtariEnv, self).__init__(**kwargs)
+        self.default_observation_shape = self.observation_space.shape
         self.masker_definitions = None
         self.all_colors = None
         self.category_lengths = None
@@ -27,9 +26,10 @@ class MaskedAtariEnv(gym.Env):
         for masker in self.masker_definitions:
             all_colors.extend([np.array(c, dtype=np.uint8) for c in masker.filter_colors])
 
-        self.all_colors = np.stack([np.ones(self.observation_space.shape, dtype=np.uint8) * c for c in all_colors])
+        self.all_colors = np.stack([np.ones(self.default_observation_shape, dtype=np.uint8) * c for c in all_colors])
         self.category_lengths = [len(masker.filter_colors) for masker in self.masker_definitions]
-        self.zero_mask = np.ones(len(self.masker_definitions), *self.observation_space.shape, dtype=np.uint8)
+        self.zero_mask = np.ones(len(self.masker_definitions), *self.default_observation_shape,
+                                 dtype=np.uint8)
 
         for i, masker_def in enumerate(self.masker_definitions):
             if zero_mask_indices is not None and i in zero_mask_indices:
@@ -45,12 +45,15 @@ class MaskedAtariEnv(gym.Env):
                 self.zero_mask[i, masker_def.row_range[0]:masker_def.row_range[1],
                                masker_def.col_range[0]:masker_def.col_range[1]] = 0
 
+        self.observation_space.shape = self.zero_mask.shape
+
     def step(self, action):
         result = super(MaskedAtariEnv, self).step(action)
         obs = result[0]
 
         all_mask_results = (obs == self.all_colors).all(axis=3)
-        category_masks = np.zeros(len(self.masker_definitions), *self.observation_space.shape, type=np.uint8)
+        category_masks = np.zeros(len(self.masker_definitions), *self.default_observation_shape,
+                                  dtype=np.uint8)
 
         current_index = 0
         for i, length in enumerate(self.category_lengths):
