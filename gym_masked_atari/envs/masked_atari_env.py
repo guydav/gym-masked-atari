8fbcef83fddd3f1dabcd88d740c26db4175df666
Guy Davidson <guydav@gmail.com>
2020-03-28 13:57:49 -0400

    Fixed the wrapper code to handle the new format, with the channels coming in the last dimension, rather than the first one. Also added support for including the pixels, both in grayscale and in color.

--- gym_masked_atari/envs/masked_atari_env.py
+++ gym_masked_atari/envs/masked_atari_env.py
@@ -1,7 +1,11 @@
-import gym
 from gym.envs import atari
 import numpy as np
+import cv2
 from .frostbite_masker import FROSTBITE_MASKER_DEFINITIONS
+
+
+INCLUDE_PIXELS_KEY = 'include_pixels'
+GRAYSCALE_PIXELS_KEY = 'grayscale_pixels'
 
 
 class MaskedAtariEnv(atari.AtariEnv):
@@ -10,6 +14,14 @@ class MaskedAtariEnv(atari.AtariEnv):
 
     def __init__(self, **kwargs):
         super(MaskedAtariEnv, self).__init__(**kwargs)
+        self.include_pixels = True
+        if INCLUDE_PIXELS_KEY in kwargs:
+            self.include_pixels = kwargs[INCLUDE_PIXELS_KEY]
+
+        self.grayscale_pixels = True
+        if GRAYSCALE_PIXELS_KEY in kwargs:
+            self.grayscale_pixels = kwargs[GRAYSCALE_PIXELS_KEY]
+
         self.default_observation_shape = self.observation_space.shape
         self.masker_definitions = None
         self.all_colors = None
@@ -29,23 +41,29 @@ class MaskedAtariEnv(atari.AtariEnv):
 
         self.all_colors = np.stack([np.ones(self.default_observation_shape, dtype=np.uint8) * c for c in all_colors])
         self.category_lengths = [len(masker.filter_colors) for masker in self.masker_definitions]
-        self.zero_mask = np.ones((len(self.masker_definitions), *self.default_observation_shape[:-1]), dtype=np.uint8)
+        self.zero_mask = np.ones((*self.default_observation_shape[:-1], len(self.masker_definitions)), dtype=np.uint8)
 
         for i, masker_def in enumerate(self.masker_definitions):
             if zero_mask_indices is not None and i in zero_mask_indices:
-                self.zero_mask[i, :, :] = 0
+                self.zero_mask[:, :, i] = 0
 
             elif masker_def.range_whitelist:
-                self.zero_mask[i, :masker_def.row_range[0], :] = 0
-                self.zero_mask[i, masker_def.row_range[1]:, :] = 0
-                self.zero_mask[i, :, :masker_def.col_range[0]] = 0
-                self.zero_mask[i, :, masker_def.col_range[1]:] = 0
+                self.zero_mask[:masker_def.row_range[0], :, i] = 0
+                self.zero_mask[masker_def.row_range[1]:, :, i] = 0
+                self.zero_mask[:, :masker_def.col_range[0], i] = 0
+                self.zero_mask[:, masker_def.col_range[1]:, i] = 0
 
             else:
-                self.zero_mask[i, masker_def.row_range[0]:masker_def.row_range[1],
-                               masker_def.col_range[0]:masker_def.col_range[1]] = 0
+                self.zero_mask[masker_def.row_range[0]:masker_def.row_range[1],
+                               masker_def.col_range[0]:masker_def.col_range[1], i] = 0
 
         self.observation_space.shape = self.zero_mask.shape
+        if self.include_pixels:
+            increment = 1
+            if not self.grayscale_pixels:
+                increment = 3
+
+            self.observation_space.shape = self.observation_space.shape[:-1] + (self.observation_space.shape[-1] + increment,)
 
     def reset(self, **kwargs):
         obs = super(MaskedAtariEnv, self).reset()
@@ -56,9 +74,9 @@ class MaskedAtariEnv(atari.AtariEnv):
         return (self._augment_observation(result[0]),) + result[1:]
 
     def _augment_observation(self, obs):
-        all_mask_results = (obs == self.all_colors).all(axis=3)
+        all_mask_results = np.moveaxis((obs == self.all_colors).all(axis=3), 0, -1)
         # print(all_mask_results.shape)
-        category_masks = np.zeros((len(self.masker_definitions), *self.default_observation_shape[:-1]), dtype=np.uint8)
+        category_masks = np.zeros((*self.default_observation_shape[:-1], len(self.masker_definitions)), dtype=np.uint8)
         # print(category_masks.shape)
 
         current_index = 0
@@ -66,10 +84,19 @@ class MaskedAtariEnv(atari.AtariEnv):
             if length > 1:
                 # print(category_masks[i].shape)
                 # print(all_mask_results[current_index: current_index + length].any(axis=0).shape)
-                category_masks[i] = all_mask_results[current_index: current_index + length].any(axis=0)
+                category_masks[:, :, i] = all_mask_results[:, :, current_index: current_index + length].any(axis=-1)
                 current_index += length
             else:
-                category_masks[i:] = all_mask_results[current_index:]
+                category_masks[:, :, i:] = all_mask_results[:, :, current_index:]
                 break
 
-        return category_masks * self.zero_mask
+        masks = category_masks * self.zero_mask
+
+        if self.include_pixels:
+            if self.grayscale_pixels:
+                obs = np.expand_dims(cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY), -1)
+
+            return np.concatenate((obs, masks), -1)
+
+        else:
+            return masks
